de893c920deaa75d898d38ce0ea5982963219713
laura34s <laurakoenigkl@gmail.com>
2025-07-24 15:14:16 -0300

    aula 6 - testes.js

---
+++ projeto-digi/aula6/testes.js
@@ -0,0 +1,12 @@
+//5 > 3
+//10 == 5
+//NAO (5 < 10)
+//6 % 2 == 0 E 6 > 3
+let number = 5
+
+console.log(5 > 3) //TRUE
+console.log(10 == 5) //FALSE
+console.log(!(5 < 10)) //false 
+console.log(((6 % 2) == 0) && (6 > 3)) // TRUE
+
+
