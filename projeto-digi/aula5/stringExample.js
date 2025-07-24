b4dff1db5da7252c1817f00853bf22b3cd92fdc2
laura34s <laurakoenigkl@gmail.com>
2025-07-24 12:48:15 -0300

    aula 5 - stringExample.js

---
+++ projeto-digi/aula5/stringExample.js
@@ -0,0 +1,13 @@
+//criar variável com string "Java Script é incrível"
+const umaString = "Java Script é incrivel";
+//acessar e imprimir a quinta letra 
+console.log (umaString.charAt(5));
+//use chartArt() para acessar o mesmo caractere
+//substitua a palavra "incrível" por "poderoso"
+console.log(umaString.replace("incrivel","poderoso"));
+//imprima o número de caracteres da string
+console.log(umaString.length);
+//use slice para extrair a palavra "Javascript"
+console.log(umaString.slice(0,10));
+//use concat() para adicionar "Vamos aprender!" ao final da string
+console.log(umaString.concat("Vamos aprender!")); 
