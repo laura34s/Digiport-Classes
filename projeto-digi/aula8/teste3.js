e8347b8eb0ae2189eba7cdca14cdb2cd37ccefc0
laura34s <laurakoenigkl@gmail.com>
2025-07-31 15:24:46 -0300

    aula 8

---
+++ projeto-digi/aula8/teste3.js
@@ -0,0 +1,11 @@
+//mostrar se o número é positivo, negativo, ou zero
+
+let num = 1;
+
+if(num == 0){
+    console.log("É zero")
+} else if (num >= 1) {
+    console.log("É positivo")
+} else {
+    console.log("É negativo")
+}
